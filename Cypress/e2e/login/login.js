05f0b6802c6bd10d5f48fbb5ad1cb1f656b1d602
silmir1996 <christianfiore10@gmail.com>
2022-10-07 01:38:13 -0300

    login.js con checkeo de elementos completos

--- Cypress/e2e/login/login.js
+++ Cypress/e2e/login/login.js
@@ -40,3 +40,16 @@ Then ('A user will be receiving a failed message', ()=> {
 Then ('A user will be receiving a blocked user message', ()=> {
     cy.findByText('Epic sadface: Sorry, this user has been locked out.').should('exist')         
 })
+
+Then ('A user will see all elements', () => {
+    cy.get('[data-test="username"]')
+    cy.get('[data-test="password"]')
+    cy.get('[data-test="login-button"]')
+    cy.get('#login_credentials > h4')
+    cy.get('#login_credentials > h4')
+    cy.get('.login_password')
+    cy.get('.login_password').should('have.text','Password for all users:secret_sauce')
+    cy.get ('#login_credentials > h4').should('have.text','Accepted usernames are:')
+    cy.get('#login_credentials').should('have.text', 'Accepted usernames are:standard_userlocked_out_userproblem_userperformance_glitch_user')
+}
+)
